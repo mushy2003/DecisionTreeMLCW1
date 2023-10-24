bdafd677c60bb3b309d37a1a88f9446dcb178bd3
mh1021 <musawer.hussain21@imperial.ac.uk>
2023-10-24 21:01:28 +0100

    minor bug fixes

--- CW1/src/evaluation.py
+++ CW1/src/evaluation.py
@@ -1,17 +1,10 @@
 import numpy as np
 
-def traverse(data_point, trained_tree):
-    if trained_tree.attribute == "leaf":
-        return trained_tree.value
-    if data_point[trained_tree.attribute] <= trained_tree.value:
-        return traverse(data_point, trained_tree.left)
-    return traverse(data_point, trained_tree.right)
-
 def confusion_matrix(y_gold, y_prediction, class_labels=None):
     if not class_labels:
         class_labels = np.unique(np.concatenate((y_gold, y_prediction)))
 
-    confusion = np.zeros((len(class_labels), len(class_labels)), dtype=np.int)
+    confusion = np.zeros((len(class_labels), len(class_labels)), dtype=int)
 
     for (i, label) in enumerate(class_labels):
         # get predictions where the ground truth is the current class label
@@ -35,22 +28,12 @@ def accuracy_from_confusion(confusion):
         return np.sum(np.diag(confusion)) / np.sum(confusion)
     else:
         return 0.
-    
-def run_test(test_data, trained_tree):
-    y_pred = np.array([])
-    for i in test_data:
-        y_pred.append(traverse(i, trained_tree))
-    return y_pred
 
-def evaluate(test_data, trained_tree):
-    y_gold = np.array([])
-    for i in test_data:
-        y_gold.append(i[-1])
-    y_pred = run_test(test_data, trained_tree)
+def evaluate(test_data_with_labels, trained_tree):
+    y_gold = test_data_with_labels[:, -1]
+    # Need to pass in the test data without the labels
+    y_pred = trained_tree.predict(test_data_with_labels[:, :-1])
 
     confusion = confusion_matrix(y_gold, y_pred, [1, 2, 3, 4])
 
     return accuracy_from_confusion(confusion)
-    
-
-
