499f19fe65dd49df93684f50ae3ed78807c29198
Oleg Tretieu <ot321@ic.ac.uk>
2023-10-26 18:26:20 +0100

    Implement basic tree plotting function

---
+++ CW1/src/visualisation.py
@@ -0,0 +1,45 @@
+from data_loader import clean_data
+from decision_tree import decision_tree_learning
+
+import matplotlib.pyplot as plt
+
+def plot_tree(root, x_coord, depth):
+    # TODO: Fix issue where text boxes overlap if the tree is too large
+    if not root:
+        return
+
+    # Display attribute and value in a text box
+    text = ""
+    if root.left or root.right:
+        text = "X" + str(root.attribute) + " < " + str(root.value)
+    else:
+        text = "leaf: " + str(root.value)
+
+    plt.text(x_coord, depth, text, verticalalignment='center', horizontalalignment='center', bbox=dict(boxstyle='round', facecolor='white', edgecolor='blue'))
+
+    # Draw the lines between nodes in the tree
+    max_width = 2 ** depth
+    points = []
+    # If root.left exists, add a point in its position so that a line is drawn
+    if root.left:
+        points.append((x_coord - max_width, depth - 1))
+    points.append((x_coord, depth))
+    # If root.right exists, add a point in its position so that a line is drawn
+    if root.right:
+        points.append((x_coord + max_width, depth - 1))
+
+    plt.plot(*zip(*points))
+
+    # Plot subtrees
+    plot_tree(root.left, x_coord - max_width, depth - 1)
+    plot_tree(root.right, x_coord + max_width, depth - 1)
+
+if __name__ == "__main__":
+    # Check tree visualisation with the clean data
+    clean_data_train = clean_data[:int(0.8 * len(clean_data))]
+
+    (node, depth) = decision_tree_learning(clean_data_train, 0)
+
+    plot_tree(node, 0, depth)
+    plt.axis('off')
+    plt.show()
